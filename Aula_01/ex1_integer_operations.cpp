5ead938c732de9d3db4d5d363955cfe4759789bd
Matheus Farina <farinamatheus07@gmail.com>
2024-02-18 14:46:32 -0300

    Subindo Repos das aulas de Estruturas de Dados da UNIVESP

---
+++ Aula_01/ex1_integer_operations.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+  int number1;
+  int number2;
+
+  std::cout << "Digite o primeiro numero: ";
+  std::cin >> number1;
+  std::cout << "Digite o segundo numero: ";
+  std::cin >> number2;
+
+  int   sum  = number1 + number2;
+  int   sub  = number1 - number2;
+  int   mul  = number1 * number2;
+  int   div  = number1 / number2;
+  float fdiv = (float)number1 / (float)number2;
+  int   res  = number1 % number2;
+
+  std::cout << "Soma:            " << sum << std::endl;
+  std::cout << "Subtracao:       " << sub << std::endl;
+  std::cout << "Multiplicacao:   " << mul << std::endl;
+  std::cout << "Divisao Inteira: " << div << std::endl;
+  std::cout << "Divisao Real:    " << fdiv << std::endl;
+  std::cout << "Resto:           " << res << std::endl;
+
+  return 0;
+}
