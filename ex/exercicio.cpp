0c06ace7a124c6e378f01d42a672d5e06c674093
Matheus Farina <farinamatheus07@gmail.com>
2024-02-18 19:06:11 -0300

    Exercicios de Lista e Fila encadeada com executavel de Palindromo

--- ex/exercicio.cpp
+++ ex/exercicio.cpp
@@ -10,6 +10,7 @@ printf ("y = %d\n" , y) ;
 return 0;
 }
 */
+/*
 #include <iostream> 	// não estava incluso no codigo do professor, causou erro de compile
 using namespace std; 	// não estava incluso no codigo do professor, causou erro de compile
 
@@ -43,4 +44,40 @@ int main()
   cout << "Average = " << avg << endl;
  
   return 0;
+}
+*/
+/*
+#include <stdio.h> 
+void sub (int *x, int y) 
+{
+    *x = 3; y = *x + 1 ;
+} 
+int main () 
+{
+    int a, b, z; a = 1; b = 2; sub (&a, b); z = a + b; printf ("%d", z); 
+    return 0 ;
+}
+*/
+/*
+ #include <stdio.h> 
+int main () 
+{
+    int x = 1, y, z; y = ++x; z = y++; y = 2; 
+    printf("%%%d, %d, %d%%", x, y, z); return 0; 
+}
+
+*/
+
+#include <stdio.h>
+
+int main ( )
+
+{
+
+int i = 3;
+
+         printf ("%d", ++ i);
+
+         return 0;
+
 }
